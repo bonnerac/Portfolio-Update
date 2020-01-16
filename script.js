59bd08440ca669590e97278da1976291c6799501
bonnerac <alex.c.bonner@gmail.com>
2020-01-15 21:22:57 -0600

    added buttons for left and right panels and added autoscroll functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+    window.scrollTo(1351, 0);
+}
+
 $("#button").click(function () {
     $('html, body').animate({
         scrollTop: 802
@@ -14,10 +18,22 @@ $("#card-contact-back").click(function () {
     }, 2000);
 })
 $("#card-about-me").click(function () {
-
-    // $('html, body').animate({
-    //     scrollLeft: 802
-    // }, 2000);
+    $('html, body').animate({
+        scrollLeft: 0
+    }, 2000);
 })
-
-
+$("#card-portfolio").click(function () {
+    $('html, body').animate({
+        scrollLeft: 2705
+    }, 2000);
+})
+$("#back-vert-two").click(function () {
+    $('html, body').animate({
+        scrollLeft: 1351
+    }, 2000);
+})
+$("#back-vert-one").click(function () {
+    $('html, body').animate({
+        scrollLeft: 1351
+    }, 2000);
+})
