74c2ea9cf2305e01e6d511fd30a69fdcdefcf33c
bonnerac <alex.c.bonner@gmail.com>
2020-01-15 19:34:51 -0600

    edited html and css to create a 3x3 grid using full-screen divs

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ $(".clicker-two").click(function () {
         scrollTop: 1604
     }, 2000);
 })
+$("#card-contact-back").click(function () {
+    $('html, body').animate({
+        scrollTop: 802
+    }, 2000);
+})
+$("#card-about-me").click(function () {
 
+    // $('html, body').animate({
+    //     scrollLeft: 802
+    // }, 2000);
+})
 
 
